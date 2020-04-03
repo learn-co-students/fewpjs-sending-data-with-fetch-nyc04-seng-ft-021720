049591dc8447ae037170575e555875b9077a7f6f
kev-kev <kevin.clark93@gmail.com>
2020-04-02 22:07:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+let formData = {
+  name: "Steve",
+  email: "steve@steve.com"
+};
+
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+let docBody = document.querySelector("body")
+function submitData(name, email){
+  return fetch("http://localhost:3000/users", configObj)
+    .then(response => response.json())
+    .then(object => docBody.append(object.id))
+    .catch(function(error){
+      alert("whoops")
+      docBody.append(error.message)
+    })
+};
